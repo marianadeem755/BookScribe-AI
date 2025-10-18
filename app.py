e0106d6658b626648944f62654f1629093279e3f
Maria Nadeem <146998477+marianadeem755@users.noreply.github.com>
2025-10-18 19:48:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import tempfile
 import pandas as pd
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 from groq import Groq
@@ -286,5 +286,6 @@ if st.session_state.current_vector_store and GROQ_API_KEY:
 st.markdown("---")
 
 st.markdown("BookScribe AI - Powered by Groq and Hugging Face")
+
 
 
